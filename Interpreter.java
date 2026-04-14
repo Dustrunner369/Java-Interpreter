9a2aa6b8c8eb0776a04b0d7a1913c61b25e2f943
Matthew <mswift0023@pointloma.edu>
2026-04-14 12:28:14 -0700

    Interpreter reads in file and prints it out.

--- Interpreter.java
+++ Interpreter.java
@@ -1,6 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Interpreter {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (!(args.length > 0)) {
+            System.out.println("Usage: java Interpreter <bytecode_file>");
+            return;
+        }
+
+        // For dev:
+        args[0] = "bytecode.txt";
+
+        File bytecodeFile = new File(args[0]);
+        try (Scanner myReader = new Scanner(bytecodeFile)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
